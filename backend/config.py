53510193dfd661d4ac460e9b234269766b3fc06c
binuri2018 <imbinuri@gmail.com>
2026-01-19 22:43:33 +0530

    deployment changes

--- backend/config.py
+++ backend/config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-change-in-production')
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://pbinu5083:aEnpsmXjdomEuR6o@cluster0.ooictsa.mongodb.net/LMS')
     DATABASE_NAME = os.getenv('DATABASE_NAME', 'lms_db')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-key-change-in-production')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'Binu2018')
     JWT_ALGORITHM = 'HS256'
     JWT_EXPIRATION_HOURS = 24
